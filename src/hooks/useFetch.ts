97911cce35828de7057140484f4f3db290fc0f0f
Jackson Silva <cajui54@gmail.com>
2024-04-27 18:46:47 -0300

    fixing bugs

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,5 +1,4 @@
-import { useState, useEffect } from "react";
-import { IItem, ItemResponse } from "../interface/item";
+import { ItemResponse } from "../interface/item";
 import { axiosInstance, endPoint } from "../helper/axios-instanse";
 import { useQuery } from "@tanstack/react-query";
 import { AxiosPromise } from "axios";
